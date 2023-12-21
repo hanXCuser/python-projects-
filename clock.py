927d71be2000ba106e44db19c6de9f361ee396f5
hanXCuser <143284975+hanXCuser@users.noreply.github.com>
2023-12-21 22:41:05 +0400

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+import time
+
+def update_clock():
+    current_time = time.strftime("%H:%M:%S")
+    hanTime.config(text = current_time)
+    hanTime.after(1000,update_clock)
+
+app = tk.Tk()
+app.title("HanTime  python")
+
+hanTime = tk.Label(app, text = "", font =("Helvetica, 48"))
+hanTime.pack()
+
+update_clock()
+app.mainloop()
+
